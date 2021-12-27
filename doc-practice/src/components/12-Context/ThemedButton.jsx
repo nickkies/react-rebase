36b99e84bb2d5c5baef8ce3a5939b667688511bb
YoonSung Kim <opqudk@gmail.com>
2021-12-27 12:55:55 +0900

    Context - rfc

--- doc-practice/src/components/12-Context/ThemedButton.jsx
+++ doc-practice/src/components/12-Context/ThemedButton.jsx
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
 
-class ThemedButton extends Component {
-  render() {
-    let props = this.props;
-    let theme = this.context;
-    return (
-      <button
-        {...props}
-        onClick={props.changeTheme}
-        style={{ backgroundColor: theme.background, color: theme.foreground }}
-      >
-        Button
-      </button>
-    );
-  }
+// class ThemedButton extends Component {
+//   render() {
+//     let props = this.props;
+//     let theme = this.context;
+//     return (
+//       <button
+//         {...props}
+//         onClick={props.changeTheme}
+//         style={{ backgroundColor: theme.background, color: theme.foreground }}
+//       >
+//         Button
+//       </button>
+//     );
+//   }
+// }
+
+// ThemedButton.contextType = ThemeContext;
+
+// export default ThemedButton;
+
+export default function ThemedButton(props) {
+  const theme = useContext(ThemeContext);
+  return (
+    <button
+      {...props}
+      onClick={props.changeTheme}
+      style={{ backgroundColor: theme.background, color: theme.foreground }}
+    >
+      Button
+    </button>
+  );
 }
-
-ThemedButton.contextType = ThemeContext;
-
-export default ThemedButton;
