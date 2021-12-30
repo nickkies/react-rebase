d96301c898ebea7812a9ed33bee8a0c640817ff5
YoonSung Kim <opqudk@gmail.com>
2021-12-30 11:27:12 +0900

    폴더 정렬을 위한 폴더 이름 변경

--- doc-practice/src/App.js
+++ doc-practice/src/App.js
@@ -4,21 +4,21 @@
 import Memo from './components/11-Memoization/Memo';
 // import Button from './components/10-HOC/Button';
 // import Input from './components/10-HOC/Input';
-// import ThankUDialog from './components/9-Composition/ThankUDialog';
-// import Dialog from './components/9-Composition/Dialog';
-// import WelcomeDialog from './components/9-Composition/WelcomeDialog';
-// import Reducer from './components/8-Hooks/Reducer';
-// import State from './components/8-Hooks/State';
-// import ControlledComponent from './components/7-Form/ControlledComponent';
-// import UncontrolledComponent from './components/7-Form/UncontrolledComponent';
-// import List from './components/6-List/List';
-// import Condition from './components/5-ConditionalRendering/Condition';
-// import Event from './components/4-Event/Event';
-// import ClassComponent from './components/2-State/ClassComponent';
-// import ClassComponent2 from './components/3-LifeCycle/ClassComponent';
-// import FunctionalComponent from './components/2-State/FunctionalComponent';
-// import Composition from './components/1-Props/Composition';
-// import { Extraction } from './components/1-Props/Extraction';
+// import ThankUDialog from './components/09-Composition/ThankUDialog';
+// import Dialog from './components/09-Composition/Dialog';
+// import WelcomeDialog from './components/09-Composition/WelcomeDialog';
+// import Reducer from './components/08-Hooks/Reducer';
+// import State from './components/08-Hooks/State';
+// import ControlledComponent from './components/07-Form/ControlledComponent';
+// import UncontrolledComponent from './components/07-Form/UncontrolledComponent';
+// import List from './components/06-List/List';
+// import Condition from './components/05-ConditionalRendering/Condition';
+// import Event from './components/04-Event/Event';
+// import ClassComponent from './components/02-State/ClassComponent';
+// import ClassComponent2 from './components/03-LifeCycle/ClassComponent';
+// import FunctionalComponent from './components/02-State/FunctionalComponent';
+// import Composition from './components/01-Props/Composition';
+// import { Extraction } from './components/01-Props/Extraction';
 
 function App() {
   return (
