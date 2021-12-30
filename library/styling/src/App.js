fd55b925a2e9c606620e98a12db1b47e6d237c46
YoonSung Kim <opqudk@gmail.com>
2021-12-30 09:24:49 +0900

    styled-component

---
+++ library/styling/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StyledComponentsEx from './components/styledComponents/StyledComponentsEx';
+
+function App() {
+  return (
+    <div className='App'>
+      <StyledComponentsEx />
+    </div>
+  );
+}
+
+export default App;
---
+++ library/styling/src/components/styledComponents/StyledComponentsEx.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function StyledComponentsEx() {
+  // Create a Title component that'll render an <h1> tag with some styles
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
+  const Button = styled.button`
+    /* Adapt the colors based on primary prop */
+    background: ${(props) => (props.primary ? 'palevioletred' : 'white')};
+    color: ${(props) => (props.primary ? 'white' : 'palevioletred')};
+
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+  `;
+
+  // A new component based on Button, but with some override styles
+  const TomatoButton = styled(Button)`
+    color: tomato;
+    border-color: tomato;
+  `;
+
+  // Use Title and Wrapper like any other React component – except they're styled!
+  return (
+    <>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+      <Button onClick={() => alert('normal')}>Normal</Button>
+      <Button onClick={() => alert('primary')} primary>
+        Primary
+      </Button>
+      <TomatoButton>Tomato</TomatoButton>
+      <br />
+      <Button as='a' href='#'>
+        Link with Button styles
+      </Button>
+      <TomatoButton as='a' href='#'>
+        Link with Tomato Button styles
+      </TomatoButton>
+    </>
+  );
+}
