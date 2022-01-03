884a92f2219500a80f66e83701d466d8b1125bf5
YoonSung Kim <opqudk@gmail.com>
2022-01-03 15:11:02 +0900

    scss overview

--- library/styling/src/components/Sass/Exemple.jsx
+++ library/styling/src/components/Sass/Exemple.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import './Example.scss';
 
 export default function Exemple() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Example</p>
+      <p className='font'>Example</p>
+      <nav>
+        <ul>
+          <li>123</li>
+          <li>
+            <a href='/'>456</a>
+          </li>
+        </ul>
+      </nav>
+      <br />
+
+      <p className='base'>base</p>
+      <p className='inverse'>inverse</p>
+      <br />
+
+      <p className='info'>info</p>
+      <p className='alert'>alert</p>
+      <p className='success'>success</p>
+      <br />
+
+      <p className='message'>message</p>
+      <p className='success2'>success2</p>
+      <p className='error'>error</p>
+      <p className='warning'>warning</p>
+      <br />
+
+      <div className='square-av'></div>
+      <div className='circle-av'></div>
+    </div>
+  );
 }
