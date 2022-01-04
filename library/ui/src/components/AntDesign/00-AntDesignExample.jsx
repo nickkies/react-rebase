f1ff9fd456345b571a68b45ed64108ac7e6b7cd8
YoonSung Kim <opqudk@gmail.com>
2022-01-04 11:13:34 +0900

    antd icon

--- library/ui/src/components/AntDesign/00-AntDesignExample.jsx
+++ library/ui/src/components/AntDesign/00-AntDesignExample.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import GetStart from './01-GetStart';
+import IconExample from './02-IconExample';
 
 export default function AntDesignExample() {
   return (
     <>
+      <IconExample />
       <GetStart />
     </>
   );
--- library/ui/src/components/AntDesign/02-IconExample.jsx
+++ library/ui/src/components/AntDesign/02-IconExample.jsx
@@ -1,5 +1,81 @@
 import React from 'react';
+import { BugOutlined } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
 
-export default function Icon() {
-  return <div></div>;
+export default function IconExample() {
+  return (
+    <>
+      <BugOutlined />
+      <Tooltip title='search'>
+        <Button type='primary' shape='circle' icon={<BugOutlined />} />
+      </Tooltip>
+      <Button type='primary' shape='circle'>
+        A
+      </Button>
+      <Button type='primary' icon={<BugOutlined />} loading>
+        Search
+      </Button>
+      <Tooltip title='search'>
+        <Button shape='circle' icon={<BugOutlined />} />
+      </Tooltip>
+      <Button icon={<BugOutlined />}>Search</Button>
+      <br />
+      <Tooltip title='search'>
+        <Button shape='circle' icon={<BugOutlined />} />
+      </Tooltip>
+      <Button icon={<BugOutlined />}>Search</Button>
+      <Tooltip title='search'>
+        <Button type='dashed' shape='circle' icon={<BugOutlined />} />
+      </Tooltip>
+      <Button type='dashed' icon={<BugOutlined />}>
+        Search
+      </Button>
+      <Button icon={<BugOutlined />} href='https://www.google.com' />
+      <br />
+      <br />
+      <Tooltip title='search'>
+        <Button
+          type='primary'
+          shape='circle'
+          icon={<BugOutlined />}
+          size='large'
+        />
+      </Tooltip>
+      <Button type='primary' shape='circle' size='large'>
+        A
+      </Button>
+      <Button type='primary' icon={<BugOutlined />} size='large'>
+        Search
+      </Button>
+      <Tooltip title='search'>
+        <Button shape='circle' icon={<BugOutlined />} size='large' />
+      </Tooltip>
+      <Button icon={<BugOutlined />} size='large'>
+        Search
+      </Button>
+      <br />
+      <Tooltip title='search'>
+        <Button shape='circle' icon={<BugOutlined />} size='large' />
+      </Tooltip>
+      <Button icon={<BugOutlined />} size='large'>
+        Search
+      </Button>
+      <Tooltip title='search'>
+        <Button
+          type='dashed'
+          shape='circle'
+          icon={<BugOutlined />}
+          size='large'
+        />
+      </Tooltip>
+      <Button type='dashed' icon={<BugOutlined />} size='large'>
+        Search
+      </Button>
+      <Button
+        icon={<BugOutlined />}
+        size='large'
+        href='https://www.google.com'
+      />
+    </>
+  );
 }
