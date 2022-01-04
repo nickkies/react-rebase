cbd366fa12e7ef681406b8dfa39d4df5906b1dfc
YoonSung Kim <opqudk@gmail.com>
2022-01-04 09:48:56 +0900

    Ant Design init

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,9 +1,16 @@
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
+// import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import AntDesignExample from './components/AntDesign/AntDesignExample';
 
 function App() {
-  return <OnsenUIExample />;
+  return (
+    <>
+      <AntDesignExample />
+      {/* <OnsenUIExample /> */}
+    </>
+  );
 }
 
 export default App;
