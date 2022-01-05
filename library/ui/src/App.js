fcc77ef949fc755300f996b923ba02d8cd7aa741
YoonSung Kim <opqudk@gmail.com>
2022-01-05 17:42:39 +0900

    Fix(rsu): "고침 Webpack module"
    package manager => 직접 다운

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import 'onsenui/css/onsen-css-components.css';
 // import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
 // import AntDesignExample from './components/AntDesign/00-AntDesignExample';
-import ReactSemanticUIExample from './components/ReactSemanticUI/ReactSemanticUIExample';
+import ReactSemanticUIExample from './components/ReactSemanticUI/00-ReactSemanticUIExample';
 
 function App() {
   return (
