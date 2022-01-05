3a0c35b5aaea52a945393177fc246de4ae0550f9
YoonSung Kim <opqudk@gmail.com>
2022-01-05 14:36:38 +0900

    antd Form

--- library/ui/src/components/AntDesign/00-AntDesignExample.jsx
+++ library/ui/src/components/AntDesign/00-AntDesignExample.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import GetStart from './01-GetStart';
 // import IconExample from './02-IconExample';
 // import TypographyExample from './03-TypographyExample';
-import LayoutExample from './04-LayoutExample';
+// import LayoutExample from './04-LayoutExample';
+import FormExample from './05-FormExample';
 
 export default function AntDesignExample() {
   return (
     <>
-      <LayoutExample />
+      <FormExample />
+      {/* <LayoutExample /> */}
       {/* <TypographyExample />
       <IconExample />
       <GetStart /> */}
---
+++ library/ui/src/components/AntDesign/05-FormExample.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox, Divider } from 'antd';
+
+const CheckboxGroup = Checkbox.Group;
+
+const plainOptions = ['Apple', 'Pear', 'Orange'];
+const defaultCheckedList = ['Apple', 'Orange'];
+
+export default function FormExample() {
+  const [checkedList, setCheckedList] = React.useState(defaultCheckedList);
+  const [indeterminate, setIndeterminate] = React.useState(true);
+  const [checkAll, setCheckAll] = React.useState(false);
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const onChange = (list) => {
+    setCheckedList(list);
+    setIndeterminate(!!list.length && list.length < plainOptions.length);
+    setCheckAll(list.length === plainOptions.length);
+  };
+
+  const onCheckAllChange = (e) => {
+    setCheckedList(e.target.checked ? plainOptions : []);
+    setIndeterminate(false);
+    setCheckAll(e.target.checked);
+  };
+
+  return (
+    <Form
+      name='basic'
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 8 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete='off'
+    >
+      <Form.Item
+        label='Username'
+        name='username'
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label='Password'
+        name='password'
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name='remember'
+        valuePropName='checked'
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type='primary' htmlType='submit'>
+          Submit
+        </Button>
+      </Form.Item>
+
+      <Checkbox
+        indeterminate={indeterminate}
+        onChange={onCheckAllChange}
+        checked={checkAll}
+      >
+        Check all
+      </Checkbox>
+      <Divider />
+      <CheckboxGroup
+        options={plainOptions}
+        value={checkedList}
+        onChange={onChange}
+      />
+    </Form>
+  );
+}
