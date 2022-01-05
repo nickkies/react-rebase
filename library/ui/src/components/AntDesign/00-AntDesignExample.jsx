de387e1ef80f1599b662f39e0edd3d5148c70e38
YoonSung Kim <opqudk@gmail.com>
2022-01-05 14:44:35 +0900

    antd Table

--- library/ui/src/components/AntDesign/00-AntDesignExample.jsx
+++ library/ui/src/components/AntDesign/00-AntDesignExample.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 // import TypographyExample from './03-TypographyExample';
 // import LayoutExample from './04-LayoutExample';
 import FormExample from './05-FormExample';
+import TableExample from './06-TableExample';
 
 export default function AntDesignExample() {
   return (
     <>
+      <TableExample />
       <FormExample />
       {/* <LayoutExample /> */}
       {/* <TypographyExample />
