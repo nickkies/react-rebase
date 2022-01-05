3bd12039d1c707e07cee1c0f8c8d55d425b5c530
YoonSung Kim <opqudk@gmail.com>
2022-01-05 16:03:24 +0900

    rsu init

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,13 +1,15 @@
+import './App.css';
 // import 'onsenui/css/onsenui.css';
 // import 'onsenui/css/onsen-css-components.css';
 // import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
-import './App.css';
-import AntDesignExample from './components/AntDesign/00-AntDesignExample';
+// import AntDesignExample from './components/AntDesign/00-AntDesignExample';
+import ReactSemanticUIExample from './components/ReactSemanticUI/ReactSemanticUIExample';
 
 function App() {
   return (
     <>
-      <AntDesignExample />
+      <ReactSemanticUIExample />
+      {/* <AntDesignExample /> */}
       {/* <OnsenUIExample /> */}
     </>
   );
