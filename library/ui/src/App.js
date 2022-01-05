273ae8b97c424cd439e67521f568e87fd460168d
YoonSung Kim <opqudk@gmail.com>
2022-01-05 10:52:36 +0900

    antd Typography

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,14 +1,14 @@
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
-import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
-// import './App.css';
-// import AntDesignExample from './components/AntDesign/00-AntDesignExample';
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
+// import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
+import './App.css';
+import AntDesignExample from './components/AntDesign/00-AntDesignExample';
 
 function App() {
   return (
     <>
-      {/* <AntDesignExample /> */}
-      <OnsenUIExample />
+      <AntDesignExample />
+      {/* <OnsenUIExample /> */}
     </>
   );
 }
--- library/ui/src/components/AntDesign/00-AntDesignExample.jsx
+++ library/ui/src/components/AntDesign/00-AntDesignExample.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GetStart from './01-GetStart';
 import IconExample from './02-IconExample';
+import TypographyExample from './03-TypographyExample';
 
 export default function AntDesignExample() {
   return (
     <>
+      <TypographyExample />
       <IconExample />
       <GetStart />
     </>
