2a0756b08353ff3e1b408bd925339f3fa0ac8d37
YoonSung Kim <opqudk@gmail.com>
2022-01-06 13:58:10 +0900

    rb Button

--- library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
+++ library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AlertExample from './01-AlertExample';
 import GridExample from './02-GridExample';
+import ButtonExample from './03-ButtonExample';
 
 export default function ReactBootstrapExample() {
   return (
     <>
+      <ButtonExample />
       <GridExample />
       <AlertExample />
     </>
