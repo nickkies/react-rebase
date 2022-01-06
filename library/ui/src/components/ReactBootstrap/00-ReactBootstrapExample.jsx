b812c13113c3edffe45283e0f46c82488a0ca47c
YoonSung Kim <opqudk@gmail.com>
2022-01-06 13:50:16 +0900

    rb Grid

--- library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
+++ library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AlertExample from './01-AlertExample';
+import GridExample from './02-GridExample';
 
 export default function ReactBootstrapExample() {
   return (
     <>
+      <GridExample />
       <AlertExample />
     </>
   );
