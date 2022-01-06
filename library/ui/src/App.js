2fce501e05fb6a21ac73bbd35d77944fbf03fc18
YoonSung Kim <opqudk@gmail.com>
2022-01-06 12:56:17 +0900

    rb init

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
+// import './App.css';
+
 // import 'onsenui/css/onsenui.css';
 // import 'onsenui/css/onsen-css-components.css';
 // import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
+
 // import AntDesignExample from './components/AntDesign/00-AntDesignExample';
-import ReactSemanticUIExample from './components/ReactSemanticUI/00-ReactSemanticUIExample';
+
+// import ReactSemanticUIExample from './components/ReactSemanticUI/00-ReactSemanticUIExample';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactBootstrapExample from './components/ReactBootstrap/00-ReactBootstrapExample';
 
 function App() {
   return (
     <>
-      <ReactSemanticUIExample />
+      <ReactBootstrapExample />
+      {/* <ReactSemanticUIExample /> */}
       {/* <AntDesignExample /> */}
       {/* <OnsenUIExample /> */}
     </>
---
+++ library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ReactBootstrapExample() {
+  return <div></div>;
+}
