182ec5c653b585f55f9b564b73dd5d812e68e8ac
YoonSung Kim <opqudk@gmail.com>
2022-01-06 11:31:27 +0900

    rsu Visibility + Progress

--- library/ui/src/components/ReactSemanticUI/00-ReactSemanticUIExample.jsx
+++ library/ui/src/components/ReactSemanticUI/00-ReactSemanticUIExample.jsx
@@ -3,10 +3,12 @@ import ButtonExampleLabeledBasic from './01-ButtonExampleLabeledBasic';
 import ListExample from './02-ListExample';
 import ShorthandPropsExample from './04-ShorthandPropsExample';
 import FormExampleFieldControlId from './05-FormExampleFieldControlId';
+import VisibilityProgressExample from './06-VisibilityProgressExample';
 
 export default function ReactSemanticUIExample() {
   return (
     <>
+      <VisibilityProgressExample />
       <FormExampleFieldControlId />
       <br />
       <ShorthandPropsExample />
