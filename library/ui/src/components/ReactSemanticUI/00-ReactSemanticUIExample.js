c2b03a4ac0527b4e19577b9ab33d3ae66aacd580
YoonSung Kim <opqudk@gmail.com>
2022-01-06 10:15:38 +0900

    rsu list

--- library/ui/src/components/ReactSemanticUI/00-ReactSemanticUIExample.js
+++ library/ui/src/components/ReactSemanticUI/00-ReactSemanticUIExample.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ButtonExampleLabeledBasic from './01-ButtonExampleLabeledBasic';
+import ListExample from './02-ListExample';
 
 export default function ReactSemanticUIExample() {
   return (
     <>
+      <ListExample />
       <ButtonExampleLabeledBasic />
     </>
   );
