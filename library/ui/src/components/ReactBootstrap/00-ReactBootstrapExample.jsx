8809d901d765a08df63c70e06dc7f420af6066e8
YoonSung Kim <opqudk@gmail.com>
2022-01-06 14:36:29 +0900

    rb Modal with Grid

--- library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
+++ library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
@@ -3,10 +3,12 @@ import AlertExample from './01-AlertExample';
 import GridExample from './02-GridExample';
 import ButtonExample from './03-ButtonExample';
 import CarouselExample from './04-CarouselExample';
+import ModalWithGridExample from './05-ModalWithGridExample';
 
 export default function ReactBootstrapExample() {
   return (
     <>
+      <ModalWithGridExample />
       <CarouselExample />
       <ButtonExample />
       <GridExample />
