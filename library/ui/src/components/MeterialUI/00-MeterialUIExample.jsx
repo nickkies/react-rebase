9aa63b932a8c2e6d49f21bb5f4542c52ff825939
YoonSung Kim <opqudk@gmail.com>
2022-01-06 15:22:06 +0900

    mui Theme

--- library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
+++ library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import GridExample from './01-GridExample';
+import ThemeExample from './02-ThemeExample';
 
 export default function MeterialUIExample() {
   return (
     <>
+      <ThemeExample />
       <GridExample />
     </>
   );
