1e59cb14e5bff2b84ad86f7b40c36a108563340c
YoonSung Kim <opqudk@gmail.com>
2022-01-06 14:59:10 +0900

    mui init

--- library/ui/src/App.js
+++ library/ui/src/App.js
@@ -1,5 +1,7 @@
 // import './App.css';
 
+import MeterialUIExample from './components/MeterialUI/00-MeterialUIExample';
+
 // import 'onsenui/css/onsenui.css';
 // import 'onsenui/css/onsen-css-components.css';
 // import OnsenUIExample from './components/OnsenUI/OnsenUIExample';
@@ -8,13 +10,14 @@
 
 // import ReactSemanticUIExample from './components/ReactSemanticUI/00-ReactSemanticUIExample';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactBootstrapExample from './components/ReactBootstrap/00-ReactBootstrapExample';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import ReactBootstrapExample from './components/ReactBootstrap/00-ReactBootstrapExample';
 
 function App() {
   return (
     <>
-      <ReactBootstrapExample />
+      <MeterialUIExample />
+      {/* <ReactBootstrapExample /> */}
       {/* <ReactSemanticUIExample /> */}
       {/* <AntDesignExample /> */}
       {/* <OnsenUIExample /> */}
---
+++ library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function MeterialUIExample() {
+  return (
+    <div>
+      <Button variant='contained'>Hello World</Button>
+    </div>
+  );
+}
