3261f9baa63643c19f6487cc98c10232ea20c504
YoonSung Kim <opqudk@gmail.com>
2022-01-06 15:25:32 +0900

    mui dark mode

--- library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
+++ library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
@@ -1,12 +1,28 @@
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import GridExample from './01-GridExample';
 import ThemeExample from './02-ThemeExample';
 
 export default function MeterialUIExample() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <ThemeExample />
       <GridExample />
-    </>
+    </ThemeProvider>
   );
 }
--- library/ui/src/components/MeterialUI/01-GridExample.jsx
+++ library/ui/src/components/MeterialUI/01-GridExample.jsx
@@ -9,7 +9,7 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  backgroundColor: '#f5f5f5',
+  //backgroundColor: '#f5f5f5',
 }));
 
 export default function GridExample() {
