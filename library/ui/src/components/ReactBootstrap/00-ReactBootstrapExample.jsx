e150fc4c81da38991c371938fb326ee938ce4f66
YoonSung Kim <opqudk@gmail.com>
2022-01-06 14:15:50 +0900

    rb Carousel

--- library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
+++ library/ui/src/components/ReactBootstrap/00-ReactBootstrapExample.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import AlertExample from './01-AlertExample';
 import GridExample from './02-GridExample';
 import ButtonExample from './03-ButtonExample';
+import CarouselExample from './04-CarouselExample';
 
 export default function ReactBootstrapExample() {
   return (
     <>
+      <CarouselExample />
       <ButtonExample />
       <GridExample />
       <AlertExample />
