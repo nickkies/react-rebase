77e94917780336f3a68d518b74454b45a00589fb
YoonSung Kim <opqudk@gmail.com>
2022-01-07 13:46:00 +0900

    twc DarkMode

--- library/ui/src/components/TailwindCSS/00-TailwindCSSExample.jsx
+++ library/ui/src/components/TailwindCSS/00-TailwindCSSExample.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Fundamentals from './01-Fundamentals';
 import ResponsiveDesign from './02-ResponsiveDesign';
+import DarkMode from './03-DarkMode';
 
 export default function TailwindCSSExample() {
   return (
     <>
+      <DarkMode />
       <ResponsiveDesign />
       <Fundamentals />
     </>
--- library/ui/tailwind.config.js
+++ library/ui/tailwind.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  darkMode: 'class',
+  // darkMode: 'media',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
