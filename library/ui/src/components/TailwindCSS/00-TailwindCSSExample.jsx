74a6c69406eb0cc6235123a540b826ceae5b4646
YoonSung Kim <opqudk@gmail.com>
2022-01-07 13:35:22 +0900

    twc ResponsiveDesign

--- library/ui/src/components/TailwindCSS/00-TailwindCSSExample.jsx
+++ library/ui/src/components/TailwindCSS/00-TailwindCSSExample.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Fundamentals from './01-Fundamentals';
+import ResponsiveDesign from './02-ResponsiveDesign';
 
 export default function TailwindCSSExample() {
   return (
     <>
+      <ResponsiveDesign />
       <Fundamentals />
     </>
   );
