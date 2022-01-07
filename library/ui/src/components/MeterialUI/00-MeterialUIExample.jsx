f9f9f4319053bbd0ca2121defa87a4b1c6609ade
YoonSung Kim <opqudk@gmail.com>
2022-01-07 11:05:10 +0900

    mui VirtaulizedList

--- library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
+++ library/ui/src/components/MeterialUI/00-MeterialUIExample.jsx
@@ -8,6 +8,7 @@ import ButtonExample from './03-ButtonExample';
 import CheckboxExample from './04-CheckboxExample';
 import DrawerExample from './05-DrawerExample';
 import StepperExample from './06-StepperExample';
+import VirtaulizedListExample from './07-VirtaulizedListExample';
 
 export default function MeterialUIExample() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -25,6 +26,8 @@ export default function MeterialUIExample() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
+      <VirtaulizedListExample />
       <StepperExample />
       <DrawerExample />
       <CheckboxExample />
