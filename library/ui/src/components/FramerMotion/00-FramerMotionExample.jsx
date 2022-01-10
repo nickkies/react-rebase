656c3f3569cb2f68355e94cdd08e9a0c1ef07d47
YoonSung Kim <opqudk@gmail.com>
2022-01-10 16:00:13 +0900

    fm SVG line drawing

--- library/ui/src/components/FramerMotion/00-FramerMotionExample.jsx
+++ library/ui/src/components/FramerMotion/00-FramerMotionExample.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AnimationExample from './01-AnimationExample';
 import KeyFrameExample from './02-KeyFrameExample';
+import SVGLine from './03-SVGLine';
 
 export default function FramerMotionExample() {
   return (
     <>
+      <SVGLine />
       <KeyFrameExample />
       <AnimationExample />
     </>
