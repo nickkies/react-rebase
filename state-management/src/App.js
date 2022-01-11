d988e3db9b3fa83c21be4f89e2ce9f08ca37da25
YoonSung Kim <opqudk@gmail.com>
2022-01-11 11:04:56 +0900

    redux Tree를 무시하는 상태관리

--- state-management/src/App.js
+++ state-management/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Counter2 from './components/Counter2';
 import Counter from './features/counter/Counter';
 
 function App() {
   return (
     <div className='App'>
       <Counter />
+      <br />
+      <br />
+      <br />
+      <Counter2 />
     </div>
   );
 }
