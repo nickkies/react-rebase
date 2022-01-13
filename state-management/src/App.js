236ded2cdbac51779d79b94d08b570ba1584a16b
YoonSung Kim <opqudk@gmail.com>
2022-01-13 14:05:29 +0900

    recoil Synchronous

--- state-management/src/App.js
+++ state-management/src/App.js
@@ -4,6 +4,7 @@ import { RecoilRoot } from 'recoil';
 // import Text from './components/RecoilExample/Text';
 // import CharacterCounter from './components/RecoilExample/CharacterCounter';
 import TodoList from './components/RecoilExample/Todo/TodoList';
+import CurrentUserInfo from './components/RecoilExample/CurrentUserInfo';
 // import TodoList from './components/TodoList';
 // import { observableTodoStore } from './app/ObservableMobxStore';
 // import MobxExample from './components/MobxExample';
@@ -13,6 +14,12 @@ function App() {
   return (
     <div className='App'>
       <RecoilRoot>
+        <CurrentUserInfo />
+
+        <br />
+        <br />
+        <br />
+
         <TodoList />
 
         {/* <FontButton />
