011ad4e205df074e6bf4ccfb052ce5a3f7680939
YoonSung Kim <opqudk@gmail.com>
2022-01-13 15:14:24 +0900

    recoil SelectorFamily

--- state-management/src/components/RecoilExample/CurrentUserInfo.jsx
+++ state-management/src/components/RecoilExample/CurrentUserInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { atom, selector, useRecoilValue } from 'recoil';
+import { atom, selector, selectorFamily, useRecoilValue } from 'recoil';
 import axios from 'axios';
 import ErrorBoundary from './ErrorBoundary';
 
-const currentUserIDState = atom({
-  key: 'CurrentUserID',
-  default: 1,
-});
+// const currentUserIDState = atom({
+//   key: 'CurrentUserID',
+//   default: 1,
+// });
 
 // const tableOfUsers = [{ name: 'nick' }, { name: 'jessy' }];
 
@@ -17,19 +17,28 @@ const currentUserIDState = atom({
 //   },
 // });
 
-const currentUserNameQuery = selector({
-  key: 'CurrentUserName',
-  get: async ({ get }) => {
-    const response = await axios.get(
-      `/api/user-name?id=${get(currentUserIDState)}`
-    );
+// const currentUserNameQuery = selector({
+//   key: 'CurrentUserName',
+//   get: async ({ get }) => {
+//     const response = await axios.get(
+//       `/api/user-name?id=${get(currentUserIDState)}`
+//     );
+
+//     return response.data.name;
+//   },
+// });
+
+const userNameQuery = selectorFamily({
+  key: 'UserName',
+  get: (userID) => async () => {
+    const response = await axios.get(`/api/user-name?id=${userID}`);
 
     return response.data.name;
   },
 });
 
 function CurrentUserUser() {
-  const userName = useRecoilValue(currentUserNameQuery);
+  const userName = useRecoilValue(userNameQuery(2));
   return <div>{userName}</div>;
 }
 
