c806863956e55fccf7bf4261aef72c5f7a50252b
YoonSung Kim <opqudk@gmail.com>
2022-01-13 10:13:57 +0900

    recoil Selector

--- state-management/src/components/RecoilExample/FontButton.jsx
+++ state-management/src/components/RecoilExample/FontButton.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { fontSizeState } from './store';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { fontSizeState, fontSizeLabelState } from './store';
 
 export default function FontButton() {
   const [fontSize, setFontSize] = useRecoilState(fontSizeState);
+  const fontSizeLabel = useRecoilValue(fontSizeLabelState);
+
   return (
-    <button
-      onClick={() => setFontSize((size) => size + 1)}
-      style={{ fontSize }}
-    >
-      Click to Enlarge
-    </button>
+    <>
+      <div>Current font size * 2: {fontSizeLabel}</div>
+      <button
+        onClick={() => setFontSize((size) => size + 1)}
+        style={{ fontSize }}
+      >
+        Click to Enlarge
+      </button>
+    </>
   );
 }
--- state-management/src/components/RecoilExample/store.js
+++ state-management/src/components/RecoilExample/store.js
@@ -1,6 +1,16 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const fontSizeState = atom({
   key: 'fontSizeState',
   default: 14,
+});
+
+export const fontSizeLabelState = selector({
+  key: 'fontSizeLabelState',
+  get: ({ get }) => {
+    const fontSize = get(fontSizeState);
+    const unit = 'px';
+
+    return `${fontSize * 2}${unit}`;
+  },
 });
