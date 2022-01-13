fac60bcf567259292aa25413650eb29b17d17bd4
YoonSung Kim <opqudk@gmail.com>
2022-01-13 16:16:36 +0900

    zustand 비교 로직  커스텀

--- state-management/src/components/ZustandExample/Text.jsx
+++ state-management/src/components/ZustandExample/Text.jsx
@@ -4,14 +4,35 @@ import create from 'zustand';
 const useStore = create((set) => ({
   fontSize: 14,
   increasefontSize: () => set((state) => ({ fontSize: state.fontSize + 1 })),
+  trigger: false,
+  toggleTrigger: () => set((state) => ({ trigger: !state.trigger })),
 }));
 
-export default function Text() {
-  const { fontSize, increasefontSize } = useStore((state) => state);
+function FontLabel() {
+  const { fontSize, increasefontSize, fontSizeLabel, toggleTrigger, trigger } =
+    useStore(
+      (state) => ({
+        ...state,
+        fontSizeLabel: state.fontSize + 'px',
+      }),
+      (oldState, newState) => oldState.trigger === newState.trigger
+    );
+
   return (
     <>
+      <div style={{ fontSize }}>Current font Size: {fontSizeLabel}</div>
       <button onClick={increasefontSize}>Size up</button>
+      <button onClick={toggleTrigger}>toggle {trigger.toString()}</button>
+    </>
+  );
+}
+
+export default function Text() {
+  const fontSize = useStore((state) => state.fontSize);
+  return (
+    <>
       <p style={{ fontSize }}>This text will increase in size too.</p>
+      <FontLabel />
     </>
   );
 }
