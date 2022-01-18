8bb42827d92478d3486d39b5fe837a37854ac1f0
YoonSung Kim <opqudk@gmail.com>
2022-01-18 22:26:43 +0900

    ReactQuery init

--- data-fetching/src/App.js
+++ data-fetching/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 // import Fetcher from './components/swr/Fetcher';
-import Mutate from './components/swr/Mutate';
-import Pagination from './components/swr/Pagination';
+// import Mutate from './components/swr/Mutate';
+// import Pagination from './components/swr/Pagination';
 // import Cache from './components/swr/Cache';
 // import Profile from './components/swr/Profile';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Example from './components/ReactQuery/Example';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className='App'>
-      <Pagination />
-      <Mutate />
+      <QueryClientProvider client={queryClient}>
+        <Example />
+      </QueryClientProvider>
+      {/* <Pagination />
+      <Mutate /> */}
       {/* <Fetcher /> */}
       {/* <Profile />
       <Cache /> */}
---
+++ data-fetching/src/components/ReactQuery/Example.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Example() {
+  return (
+    <>
+      <Page />
+    </>
+  );
+}
+
+const Page = () => {
+  return <>Page</>;
+};
