53ec595426449373656edd154c2c87e485e1583d
YoonSung Kim <opqudk@gmail.com>
2022-01-18 14:41:36 +0900

    swr Pagination

--- data-fetching/src/App.js
+++ data-fetching/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import Fetcher from './components/swr/Fetcher';
 import Mutate from './components/swr/Mutate';
+import Pagination from './components/swr/Pagination';
 // import Cache from './components/swr/Cache';
 // import Profile from './components/swr/Profile';
 
 function App() {
   return (
     <div className='App'>
+      <Pagination />
       <Mutate />
       {/* <Fetcher /> */}
       {/* <Profile />
--- data-fetching/src/mocks/handlers.js
+++ data-fetching/src/mocks/handlers.js
@@ -11,4 +11,18 @@ export const handlers = [
     );
     // return res(ctx.status(400));
   }),
+  rest.get('http://localhost:3000/api/users', async (req, res, ctx) => {
+    const pageIndex = req.url.searchParams.get('page');
+
+    return res(
+      ctx.json([
+        { id: `1 ${pageIndex}`, name: `nick 1-${pageIndex}` },
+        { id: `2 ${pageIndex}`, name: `nick 2-${pageIndex}` },
+        { id: `3 ${pageIndex}`, name: `nick 3-${pageIndex}` },
+        { id: `4 ${pageIndex}`, name: `nick 4-${pageIndex}` },
+        { id: `5 ${pageIndex}`, name: `nick 5-${pageIndex}` },
+      ])
+    );
+    // return res(ctx.status(400));
+  }),
 ];
