781060dfb12379db7be17ba0409483283e521a3e
YoonSung Kim <opqudk@gmail.com>
2022-01-24 10:41:16 +0900

    Next(Docs): "API Routes"

--- next.js/nextjs-blog/pages/api/hello.js
+++ next.js/nextjs-blog/pages/api/hello.js
@@ -1,3 +1,6 @@
 export default function handler(req, res) {
-  res.status(200).json({ text: 'Hello' });
+  // 서버쪽에서 찍힘
+  console.log('/api/hello');
+  // res.status(200).json({ text: 'Hello' });
+  res.redirect(307, '/api/bye');
 }
