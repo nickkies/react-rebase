b1a5b24dc421df9e5335288c664e72369afe0e63
YoonSung Kim <opqudk@gmail.com>
2022-01-30 17:01:44 +0900

    common-ui(carousel): "추가 carousel"

--- playground/common-ui/src/components/carousel/Carousel.tsx
+++ playground/common-ui/src/components/carousel/Carousel.tsx
@@ -1,5 +1,125 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { RiArrowDropLeftLine, RiArrowDropRightLine } from 'react-icons/ri';
+import { useState } from 'react';
+
+const Base = styled.div``;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const ArrowButton = styled.button<{ pos: 'left' | 'right' }>`
+  position: absolute;
+  top: 50%;
+  z-index: 1;
+  padding: 8px 12px;
+  font-size: 48px;
+  font-weight: bold;
+  background-color: transparent;
+  color: #fff;
+  border: none;
+  margin: 0;
+  cursor: pointer;
+  ${({ pos }) =>
+    pos === 'left'
+      ? css`
+          left: 0;
+        `
+      : css`
+          right: 0;
+        `}
+`;
+
+const CarouselList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  overflow: hidden;
+`;
+
+const CarouselListItem = styled.li<{ activeIndex: number }>`
+  width: 100%;
+  flex: 1 0 100%;
+  transform: translateX(-${({ activeIndex }) => activeIndex * 100}%);
+  transition: 200ms ease;
+  > img {
+    width: 100%;
+    height: fit-content;
+  }
+`;
+
+const NavButton = styled.button<{ isActive?: boolean }>`
+  width: 4px;
+  height: 4px;
+  background-color: #000;
+  opacity: ${({ isActive }) => (isActive ? 0.3 : 0.1)};
+`;
+
+const Nav = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  > li + li {
+    margin-left: 4px;
+  }
+`;
+
+const NavItem = styled.li`
+  display: inline-block;
+`;
+
+const banners = [
+  'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e2dc50905%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e2dc50905%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.40625%22%20y%3D%22221.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+  'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e2dc50908%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e2dc50908%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23282c34%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22246.828125%22%20y%3D%22221.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+  'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e2dc5090b%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e2dc5090b%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22274.3046875%22%20y%3D%22221.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+];
+
 const Carousel: React.FC = () => {
-  return <>carousel</>;
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  const handleNext = () => {
+    setActiveIndex((prev) => (prev + 1) % banners.length);
+  };
+
+  const handlePrev = () => {
+    setActiveIndex((prev) => (prev - 1 + banners.length) % banners.length);
+  };
+
+  const goTo = (idx: number) => {
+    setActiveIndex(idx);
+  };
+
+  return (
+    <Base>
+      <Container>
+        <ArrowButton pos='left' onClick={handlePrev}>
+          <RiArrowDropLeftLine />
+        </ArrowButton>
+        <CarouselList>
+          {banners.map((banner, idx) => (
+            <CarouselListItem key={idx} activeIndex={activeIndex}>
+              <img src={banner} alt='caroucel img' />
+            </CarouselListItem>
+          ))}
+        </CarouselList>
+        <ArrowButton pos='right' onClick={handleNext}>
+          <RiArrowDropRightLine />
+        </ArrowButton>
+      </Container>
+      <Nav>
+        {Array.from({ length: banners.length }).map((_, idx) => (
+          <NavItem key={idx} onClick={() => goTo(idx)}>
+            <NavButton isActive={activeIndex === idx} />
+          </NavItem>
+        ))}
+      </Nav>
+    </Base>
+  );
 };
 
 export default Carousel;
