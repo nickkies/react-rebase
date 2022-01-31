2b8e2e731d9d88c2d46f8bd96053eac749dc87cf
YoonSung Kim <opqudk@gmail.com>
2022-01-31 16:41:23 +0900

    common-ui(pagination): "design - emotion"

--- playground/common-ui/src/components/Pagination/Pagination.tsx
+++ playground/common-ui/src/components/Pagination/Pagination.tsx
@@ -1,3 +1,7 @@
+import styled from '@emotion/styled/macro';
+import { GrFormPrevious, GrFormNext } from 'react-icons/gr';
+import { AiOutlineEllipsis } from 'react-icons/ai';
+
 import usePagination from '../../hooks/usePagination';
 
 interface Props {
@@ -9,20 +13,63 @@ interface Props {
   boundaryCount?: number;
 }
 
+const Navigation = styled.nav``;
+
+const Button = styled.button<{ selected?: boolean }>`
+  color: ${({ selected }) => (selected ? '#fff' : '#000')};
+  border: 0;
+  margin: 0;
+  padding: 8px 12px;
+  font-size: 16px;
+  font-weight: normal;
+  background-color: ${({ selected }) => (selected ? '#36dafa' : '#fff')};
+  cursor: pointer;
+  border-radius: 100%;
+  width: 48px;
+  height: 48px;
+  $:hover {
+    background-color: #ccc;
+    color: #fff;
+  }
+  &:active {
+    opacity: 0.8;
+  }
+`;
+
+const ItemList = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  list-style: none;
+  $ > li + li {
+    margin-left: 8px;
+  }
+`;
+
+const Item = styled.li``;
+
 const Pagination: React.FC<Props> = (props) => {
   const { items } = usePagination(props);
+
+  const getLabel = (item: number | string) => {
+    if (typeof item === 'number') return item;
+    else if (item.indexOf('ellipsis') > -1) return <AiOutlineEllipsis />;
+    else if (item.indexOf('prev') > -1) return <GrFormPrevious />;
+    else if (item.indexOf('next') > -1) return <GrFormNext />;
+  };
+
   return (
-    <nav>
-      <ul>
+    <Navigation>
+      <ItemList>
         {items.map(({ key, disabled, selected, onClick, item }) => (
-          <li key={key}>
-            <button disabled={disabled} onClick={onClick}>
-              {item}
-            </button>
-          </li>
+          <Item key={key}>
+            <Button disabled={disabled} onClick={onClick} selected={selected}>
+              {getLabel(item)}
+            </Button>
+          </Item>
         ))}
-      </ul>
-    </nav>
+      </ItemList>
+    </Navigation>
   );
 };
 
