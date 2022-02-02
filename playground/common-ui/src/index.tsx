ddc622135462c7bd42d05c9342ee705de7db0741
YoonSung Kim <opqudk@gmail.com>
2022-02-02 20:19:32 +0900

    common-ui(infinite-scroll): "init"
    pagination과 같은 api setting

--- playground/common-ui/src/index.tsx
+++ playground/common-ui/src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 // import SkeletonApp from './SkeletonApp';
 // import CarouselApp from './CarouselApp';
 // import PaginationApp from './PaginationApp';
-import ModalApp from './ModalApp';
+// import ModalApp from './ModalApp';
+import InfiniteScrollApp from './InfiniteScrollApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ModalApp />
+    <InfiniteScrollApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
