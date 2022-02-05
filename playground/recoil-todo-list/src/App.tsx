62d436dc9ea69824a3cb81873ab7efdf91f1d9ef
YoonSung Kim <opqudk@gmail.com>
2022-02-05 09:46:18 +0900

    recoil-todo-list: "할일 입력 모달"

--- playground/recoil-todo-list/src/App.tsx
+++ playground/recoil-todo-list/src/App.tsx
@@ -1,10 +1,12 @@
 import { RecoilRoot } from 'recoil';
 import Calendar from './components/Calendar';
+import TodoFormModal from './features/TodoFormModal';
 
 function App() {
   return (
     <RecoilRoot>
       <Calendar />
+      <TodoFormModal />
     </RecoilRoot>
   );
 }
---
+++ playground/recoil-todo-list/src/features/TodoFormModal/index.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled/macro';
+import { useState } from 'react';
+import Modal from '../../components/Modal';
+
+const ModalBody = styled.div`
+  width: 100vw;
+  max-width: 386px;
+  padding: 8px;
+`;
+
+const Card = styled.div`
+  width: 100%;
+  max-width: 370px;
+  border-radius: 16px;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+  padding: 24px;
+  box-sizing: border-box;
+  background-color: #19181a;
+  small + input {
+    margin-top: 24px;
+  }
+`;
+
+const Date = styled.small`
+  display: block;
+  color: #c9c8cc;
+`;
+
+const InputTodo = styled.input`
+  padding: 16px 24px;
+  border: none;
+  width: 100%;
+  box-sizing: border-box;
+  background-color: transparent;
+  color: #c9c8cc;
+  caret-color: #c9c8cc;
+`;
+
+const TodoFormModal: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Modal isOpen={true} onClose={handleClose}>
+      <ModalBody>
+        <Card>
+          <Date>2022-02-02</Date>
+          <InputTodo placeholder='새로운 이벤트' />
+        </Card>
+      </ModalBody>
+    </Modal>
+  );
+};
+
+export default TodoFormModal;
