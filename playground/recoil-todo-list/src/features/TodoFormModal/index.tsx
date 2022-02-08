2061f4782ad5b2388220828fa0c8938ba7c8c123
YoonSung Kim <opqudk@gmail.com>
2022-02-08 13:41:58 +0900

    recoil-todo-list(todoFormModal): "atom으로 상태관리"

--- playground/recoil-todo-list/src/features/TodoFormModal/index.tsx
+++ playground/recoil-todo-list/src/features/TodoFormModal/index.tsx
@@ -1,6 +1,13 @@
 import styled from '@emotion/styled/macro';
-import { useState } from 'react';
+import React, { useState, useRef } from 'react';
+
+import { useRecoilCallback, useRecoilState, useRecoilValue } from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
+
+import { todoFormModalOpenState } from './atom';
 import Modal from '../../components/Modal';
+import { selectedDateState, todoListState } from '../TodoList/atom';
+import { getSimpleDateFormat } from '../../utils';
 
 const ModalBody = styled.div`
   width: 100vw;
@@ -37,18 +44,65 @@ const InputTodo = styled.input`
 `;
 
 const TodoFormModal: React.FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [todo, setTodo] = useState<string>('');
+
+  const selectedDate = useRecoilValue(selectedDateState);
+  const todoList = useRecoilValue(todoListState);
+
+  const [isOpen, setIsOpen] = useRecoilState(todoFormModalOpenState);
+
+  const reset = () => {
+    setTodo('');
+    inputRef.current?.focus();
+  };
 
   const handleClose = () => {
     setIsOpen(false);
+  };
+
+  const addTodo = useRecoilCallback(
+    ({ snapshot, set }) =>
+      () => {
+        const todoList = snapshot.getLoadable(todoListState).getValue();
+
+        const newTodo = {
+          id: uuidv4(),
+          content: todo,
+          done: false,
+          date: selectedDate,
+        };
+
+        set(todoListState, [...todoList, newTodo]);
+      },
+    [todo, selectedDate, todoList]
+  );
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      addTodo();
+      reset();
+      handleClose();
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value);
   };
 
   return (
     <Modal isOpen={isOpen} onClose={handleClose}>
       <ModalBody>
         <Card>
-          <Date>2022-02-02</Date>
-          <InputTodo placeholder='새로운 이벤트' />
+          <Date>{getSimpleDateFormat(selectedDate)}</Date>
+          <InputTodo
+            ref={inputRef}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            value={todo}
+            placeholder='새로운 이벤트'
+          />
         </Card>
       </ModalBody>
     </Modal>
