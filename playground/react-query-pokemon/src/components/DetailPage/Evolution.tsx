c0df9dde73d809466bf3e2fab898bd301990ef51
YoonSung Kim <opqudk@gmail.com>
2022-02-12 17:23:05 +0900

    react-query-pokemon: "Evolution 구조"

--- playground/react-query-pokemon/src/components/DetailPage/Evolution.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/Evolution.tsx
@@ -1,5 +1,21 @@
+import styled from '@emotion/styled/macro';
+import { EvolutionStage } from '.';
+
+const Base = styled.div``;
+
+const Title = styled.h4``;
+
+const List = styled.ul``;
+
 const Evolution: React.FC = () => {
-  return <>Evolution</>;
+  return (
+    <Base>
+      <Title>Evolution</Title>
+      <List>
+        <EvolutionStage />
+      </List>
+    </Base>
+  );
 };
 
 export default Evolution;
--- playground/react-query-pokemon/src/components/DetailPage/EvolutionStage.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/EvolutionStage.tsx
@@ -1,5 +1,32 @@
+import styled from '@emotion/styled/macro';
+
+const Base = styled.li``;
+
+const ImageWrapper = styled.div``;
+
+const Image = styled.img``;
+
+const DividerWrapper = styled.div``;
+
+const Text = styled.div``;
+
+const Divider = styled.div``;
+
 const EvolutionStage: React.FC = () => {
-  return <>EvolutionStage</>;
+  return (
+    <Base>
+      <ImageWrapper>
+        <Image src='' />
+      </ImageWrapper>
+      <DividerWrapper>
+        <Text>Level 11</Text>
+        <Divider />
+      </DividerWrapper>
+      <ImageWrapper>
+        <Image src='' />
+      </ImageWrapper>
+    </Base>
+  );
 };
 
 export default EvolutionStage;
