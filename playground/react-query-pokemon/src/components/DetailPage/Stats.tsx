cd4d1230bcd9ef638a033b44f84f545293160914
YoonSung Kim <opqudk@gmail.com>
2022-02-12 17:15:14 +0900

    react-query-pokemon: "Stats 구조"

--- playground/react-query-pokemon/src/components/DetailPage/Stats.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/Stats.tsx
@@ -1,5 +1,36 @@
+import styled from '@emotion/styled/macro';
+
+const Base = styled.div``;
+
+const Title = styled.h4``;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
+
+const Name = styled.div``;
+
+const Amount = styled.div``;
+
+const GaugeWrapper = styled.div``;
+
+const Gauge = styled.div``;
+
 const Stats: React.FC = () => {
-  return <>Stats</>;
+  return (
+    <Base>
+      <Title>Base Stats</Title>
+      <List>
+        <ListItem>
+          <Name>HP</Name>
+          <Amount>50</Amount>
+          <GaugeWrapper>
+            <Gauge />
+          </GaugeWrapper>
+        </ListItem>
+      </List>
+    </Base>
+  );
 };
 
 export default Stats;
