9dce215cca11027014a1dd74516a8b338f7bfb64
YoonSung Kim <opqudk@gmail.com>
2022-02-12 17:04:46 +0900

    react-query-pokemon: "About 구조"

--- playground/react-query-pokemon/src/components/DetailPage/Abilities.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/Abilities.tsx
@@ -1,5 +1,29 @@
+import styled from '@emotion/styled/macro';
+
+const Base = styled.div``;
+
+const Title = styled.h4``;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
+
+const Label = styled.span``;
+
+const Description = styled.span``;
+
 const Abilities: React.FC = () => {
-  return <>Abilities</>;
+  return (
+    <Base>
+      <Title>Abilities</Title>
+      <List>
+        <ListItem>
+          <Label>Label</Label>
+          <Description>Description</Description>
+        </ListItem>
+      </List>
+    </Base>
+  );
 };
 
 export default Abilities;
--- playground/react-query-pokemon/src/components/DetailPage/About.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/About.tsx
@@ -1,5 +1,81 @@
+import styled from '@emotion/styled/macro';
+import { Abilities } from '.';
+
+const Base = styled.article``;
+
+const FlavorText = styled.p``;
+
+const TypeWrapper = styled.div``;
+
+const TypeList = styled.div``;
+
+const TypeImage = styled.img``;
+
+const TypeLabel = styled.span``;
+
+const Title = styled.h4``;
+
+const InfoContainerWrapper = styled.div``;
+
+const InfoContainer = styled.div``;
+
+const InfoItem = styled.div``;
+
+const InfoItemLabel = styled.span``;
+
+const InfoItemValue = styled.span``;
+
 const About: React.FC = () => {
-  return <>About</>;
+  return (
+    <Base>
+      <FlavorText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </FlavorText>
+      <TypeList>
+        <TypeWrapper>
+          <TypeImage src='/assets/fire.svg' />
+          <TypeLabel>FIRE</TypeLabel>
+        </TypeWrapper>
+        <TypeWrapper>
+          <TypeImage src='/assets/grass.svg' />
+          <TypeLabel>GRASS</TypeLabel>
+        </TypeWrapper>
+      </TypeList>
+
+      <InfoContainerWrapper>
+        <Title>Pokédex Data</Title>
+        <InfoContainer>
+          <InfoItem>
+            <InfoItemLabel>Height</InfoItemLabel>
+            <InfoItemValue>100m</InfoItemValue>
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Weight</InfoItemLabel>
+            <InfoItemValue>100kg</InfoItemValue>
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Gender</InfoItemLabel>
+            <InfoItemValue>Male</InfoItemValue>
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Growth Rate</InfoItemLabel>
+            <InfoItemValue>medium-slow</InfoItemValue>
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Base Exp</InfoItemLabel>
+            <InfoItemValue>100</InfoItemValue>
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Rarity</InfoItemLabel>
+            <InfoItemValue>Normal</InfoItemValue>
+          </InfoItem>
+        </InfoContainer>
+      </InfoContainerWrapper>
+
+      <Abilities />
+    </Base>
+  );
 };
 
 export default About;
