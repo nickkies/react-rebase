fa848b747b97d81287bbf80dbafff48b1745fedd
YoonSung Kim <opqudk@gmail.com>
2022-02-13 16:10:12 +0900

    react-query-pokemon(Design): "Evolution"

--- playground/react-query-pokemon/src/components/DetailPage/Evolution.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/Evolution.tsx
@@ -1,16 +1,34 @@
 import styled from '@emotion/styled/macro';
 import { EvolutionStage } from '.';
 
-const Base = styled.div``;
+const Base = styled.div`
+  margin-top: 32px;
+  padding: 0 20px 20px;
+`;
 
-const Title = styled.h4``;
+const Title = styled.h4<{ color: string }>`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const List = styled.ul``;
+const List = styled.ul`
+  margin: 20px 0 0 0;
+  padding: 0;
+  > li + li {
+    margin-top: 24px;
+  }
+`;
 
 const Evolution: React.FC = () => {
   return (
     <Base>
-      <Title>Evolution</Title>
+      <Title color='green'>Evolution</Title>
+      <List>
+        <EvolutionStage />
+      </List>
       <List>
         <EvolutionStage />
       </List>
--- playground/react-query-pokemon/src/components/DetailPage/EvolutionStage.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/EvolutionStage.tsx
@@ -1,16 +1,39 @@
 import styled from '@emotion/styled/macro';
 
-const Base = styled.li``;
+const Base = styled.li`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const ImageWrapper = styled.div``;
 
-const Image = styled.img``;
+const Image = styled.img`
+  widht: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
 
-const DividerWrapper = styled.div``;
+const DividerWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
-const Text = styled.div``;
+const Text = styled.div<{ color: string }>`
+  text-align: center;
+  color: ${({ color }) => color};
+  font-size: 12px;
+`;
 
-const Divider = styled.div``;
+const Divider = styled.div`
+  background-color: #d1d5db;
+  border-radius: 12px;
+  height: 8px;
+  margin-inline: 8px;
+  margin-top: 4px;
+`;
 
 const EvolutionStage: React.FC = () => {
   return (
@@ -19,7 +42,7 @@ const EvolutionStage: React.FC = () => {
         <Image src='' />
       </ImageWrapper>
       <DividerWrapper>
-        <Text>Level 11</Text>
+        <Text color='green'>Level 11</Text>
         <Divider />
       </DividerWrapper>
       <ImageWrapper>
