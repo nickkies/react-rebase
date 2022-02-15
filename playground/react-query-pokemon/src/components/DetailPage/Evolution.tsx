faad04e624d36b02712dd2b493432d9c6902bac9
YoonSung Kim <opqudk@gmail.com>
2022-02-15 17:19:09 +0900

    react-query-pokemon: "재귀함수로 진화배열 생성"

--- playground/react-query-pokemon/src/components/DetailPage/Evolution.tsx
+++ playground/react-query-pokemon/src/components/DetailPage/Evolution.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled/macro';
+import { useEffect, useState } from 'react';
+
 import useEvolutionChain from 'hooks/useEvolutionChain';
-import { Color } from 'types';
+import { Chain, Color } from 'types';
 import { EvolutionStage } from '.';
 
 type Props = {
@@ -30,19 +32,48 @@ const List = styled.ul`
 `;
 
 const Evolution: React.FC<Props> = ({ url, color }) => {
-  const { data } = useEvolutionChain(url);
+  const { isSuccess, data } = useEvolutionChain(url);
 
-  console.dir(data);
+  const [evolutionChain, setEvolutionChain] = useState<
+    Array<{
+      from: { name: string; url: string };
+      to: { name: string; url: string };
+      level: number;
+    }>
+  >([]);
+
+  useEffect(() => {
+    const makeEvolutionChain = (chain: Chain) => {
+      if (chain.evolves_to.length) {
+        const [evolvesTo] = chain.evolves_to;
+
+        const from = chain.species;
+        const to = evolvesTo.species;
+        const level = evolvesTo.evolution_details[0].min_level;
+
+        setEvolutionChain((prev) => [...prev, { from, to, level }]);
+
+        makeEvolutionChain(chain.evolves_to[0]);
+      }
+    };
+
+    isSuccess && data && makeEvolutionChain(data.data.chain);
+
+    return (): void => {
+      setEvolutionChain([]);
+    };
+  }, [isSuccess, data]);
 
   return (
     <Base>
       <Title color='green'>Evolution</Title>
-      <List>
-        <EvolutionStage />
-      </List>
-      <List>
-        <EvolutionStage />
-      </List>
+      {evolutionChain.length && (
+        <List>
+          {evolutionChain.map(({ from, to, level }, idx) => (
+            <EvolutionStage key={idx} />
+          ))}
+        </List>
+      )}
     </Base>
   );
 };
