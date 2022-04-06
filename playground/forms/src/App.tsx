a8e4b14f637003bb994c05f2062dd5ec4b25befe
YoonSung Kim <opqudk@gmail.com>
2022-04-06 16:29:38 +0900

    Feat: "FormContext"

--- playground/forms/src/App.tsx
+++ playground/forms/src/App.tsx
@@ -1,9 +1,12 @@
 import ControlledWrapper from './Components/ControlledWrapper';
+import FormContext from './Components/FormContext';
 
 function App() {
   return (
     <div className='App'>
       <ControlledWrapper />
+      <br />
+      <FormContext />
     </div>
   );
 }
