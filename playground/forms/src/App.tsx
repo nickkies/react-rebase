c2d73dafe9c88f54f8cada6f81f2a27fba6aa719
YoonSung Kim <opqudk@gmail.com>
2022-04-08 13:55:35 +0900

    Feat: "watch"

--- playground/forms/src/App.tsx
+++ playground/forms/src/App.tsx
@@ -1,6 +1,7 @@
 import ControlledWrapper from './Components/ControlledWrapper';
 import FormContext from './Components/FormContext';
 import RegisterFields from './Components/RegisterFields';
+import Watch from './Components/Watch';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <FormContext />
       <br />
       <RegisterFields />
+      <br />
+      <Watch />
     </div>
   );
 }
