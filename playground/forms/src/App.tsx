0c5f632c087fc8d0570fc95192be51e4847cd63a
YoonSung Kim <opqudk@gmail.com>
2022-04-08 14:38:33 +0900

    Feat: "Validation"

--- playground/forms/src/App.tsx
+++ playground/forms/src/App.tsx
@@ -1,11 +1,14 @@
 import ControlledWrapper from './Components/ControlledWrapper';
 import FormContext from './Components/FormContext';
 import RegisterFields from './Components/RegisterFields';
+import Validation from './Components/Validation';
 import Watch from './Components/Watch';
 
 function App() {
   return (
     <div className='App'>
+      <Validation />
+      <br />
       <ControlledWrapper />
       <br />
       <FormContext />
