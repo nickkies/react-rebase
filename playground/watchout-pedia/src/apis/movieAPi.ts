f2668bac646aae95c491c1d2b751ec8786a38b24
YoonSung Kim <opqudk@gmail.com>
2022-05-23 14:59:31 +0900

    Feat: "Add apis"

---
+++ playground/watchout-pedia/src/apis/movieAPi.ts
@@ -0,0 +1,18 @@
+import axiosInstance from 'apis';
+
+export const latestApi = () => axiosInstance.get('/movie/latest');
+
+export const upcommingApi = () => axiosInstance.get('/movie/upcomming');
+
+export const nowPlayingApi = () => axiosInstance.get('/movie/top_rate');
+
+export const popularApi = () => axiosInstance.get('/movie/popular');
+
+export const detailApi = (movieId: string) =>
+  axiosInstance.get(`/movie/${movieId}`);
+
+export const similarApi = (movieId: string) =>
+  axiosInstance.get(`/movie/${movieId}/similar`);
+
+export const searchApi = (query: string) =>
+  axiosInstance.get(`/search/movie?query?=${query}`);
