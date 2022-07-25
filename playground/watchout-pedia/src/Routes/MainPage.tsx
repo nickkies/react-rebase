85fed8d2b2ecf1f5104433a866c640fe1dc7ef5c
YoonSung Kim <opqudk@gmail.com>
2022-07-25 16:52:55 +0900

    Feat: "Add main, card"

--- playground/watchout-pedia/src/Routes/MainPage.tsx
+++ playground/watchout-pedia/src/Routes/MainPage.tsx
@@ -1,13 +1,29 @@
+import styled from '@emotion/styled';
+
 import { Footer, Header } from 'Components';
 import LatestMovieSection from 'features/movie/Latest';
 
+const Main = styled.main`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Container = styled.div`
+  margin-top: 62px;
+  padding: 24px 0;
+`;
+
 const MainPage: React.FC = () => {
   return (
-    <div>
+    <>
       <Header />
-      <LatestMovieSection />
+      <Main>
+        <Container>
+          <LatestMovieSection />
+        </Container>
+      </Main>
       <Footer />
-    </div>
+    </>
   );
 };
 
