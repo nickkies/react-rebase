865409b91db2f72e191991307f15127affc759ca
YoonSung Kim <opqudk@gmail.com>
2022-07-25 16:00:56 +0900

    Feat: "Add fetch latest moive"

--- playground/watchout-pedia/src/Routes/MainPage.tsx
+++ playground/watchout-pedia/src/Routes/MainPage.tsx
@@ -1,9 +1,11 @@
 import { Footer, Header } from 'Components';
+import LatestMovieSection from 'features/movie/Latest';
 
 const MainPage: React.FC = () => {
   return (
     <div>
       <Header />
+      <LatestMovieSection />
       <Footer />
     </div>
   );
